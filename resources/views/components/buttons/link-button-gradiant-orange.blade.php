98a3e8568002285f99ece70780e6443b782820de
mina remon <minakiroollos@gmail.com>
2025-07-18 17:38:38 +0300

    Update orange gradient link button component to include href attribute
    - Sets explicit href attribute using the 'link' prop in the <a> tag
    - Ensures proper navigation for button links
    - Keeps support for all rounded and text color props
    - No changes to existing CSS or class merging logic
    - Fixes navigation and accessibility for the gradient-orange button component

--- resources/views/components/buttons/link-button-gradiant-orange.blade.php
+++ resources/views/components/buttons/link-button-gradiant-orange.blade.php
@@ -8,6 +8,7 @@
     'text_white' => true
 ])
 
-<a {{ $attributes->class(['rounded-sm' => $rounded_sm, 'rounded-lg' => $rounded_lg , 'rounded-md' => $rounded_md , '$rounded_xl' => $rounded_xl , 'text-white' => $text_white])->merge(['class' => 'bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800 py-3 px-4 text-center font-medium']) }})>
+
+<a href="{{$link}}" {{ $attributes->class(['rounded-sm' => $rounded_sm, 'rounded-lg' => $rounded_lg , 'rounded-md' => $rounded_md , '$rounded_xl' => $rounded_xl , 'text-white' => $text_white])->merge(['class' => 'bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800 py-3 px-4 text-center font-medium']) }})>
     {{ $content }}
 </a>
