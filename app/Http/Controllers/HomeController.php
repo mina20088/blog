9ece92fc71f77a4109df5b31e5e61989a6354416
mina remon <minakiroollos@gmail.com>
2025-08-08 14:21:39 +0300

    feat: Implement search functionality in HomeController and update home view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,18 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Mail\UnsubscribeMail;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 
 class HomeController extends Controller
 {
-    public function home(){
-        $widgets = [
-            ['type' => 'stats-card', 'title' => 'Users', 'value' => 152],
-            ['type' => 'chart-card', 'title' => 'Sales Chart', 'chartId' => 'salesChart'],
-            ['type' => 'activity-card', 'title' => 'Recent Activity', 'items' => []],
-        ];
-        return view("home" ,['widgets' => $widgets]);
+    public function home(Request $request){
+        $searchQuery = $request->query('search');
+        $searchByQuery = $request->query('searchBy');
+        dump($request->query());
+
+        $users = User::when($searchQuery && $searchByQuery, static function ($query) use ($searchQuery, $searchByQuery) {
+            foreach ($searchByQuery as $searchBy) {
+                $query->where($searchBy , 'LIKE', "%{$searchQuery}%");
+            }
+        })->get();
+
+        return view("home" , ['users' => $users]);
     }
 
     public function about(){
