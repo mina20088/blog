092fba59c395306326315a6f2f9b0d55c0ecace4
mina remon <minakiroollos@gmail.com>
2025-08-12 10:56:06 +0300

    feat: Add filter icon to the users page button for enhanced visual clarity

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -6,7 +6,10 @@
 @section('content')
     <div class="flex justify-between items-center my-4 sm:my-9 ">
         <h1 class="font-bold text-2xl">Users</h1>
-        <button class="border border-1 xs:px-4 xs:py-4 rounded-lg">Filters/Search</button>
+        <button class="flex gap-1 border border-1 xs:px-4 xs:py-4 rounded-lg items-center">
+            <span><x-svgs.filter class="w-4"/></span>
+            <span>Filters/Search</span>
+        </button>
     </div>
 
     <div class="border bg-gray-200 xs:py-3 xs:px-3 lg:py-5 lg:px-5 rounded-lg">
