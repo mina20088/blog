220b021540a73d8258e5077b0ed082986d27a005
mina remon <minakiroollos@gmail.com>
2025-08-12 11:41:13 +0300

    feat: Implement animated filters/search toggle in users dashboard for improved UX

--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -4,30 +4,36 @@
 
 
 @section('content')
-    <div class="flex justify-between items-center my-4 sm:my-9 ">
-        <h1 class="font-bold text-2xl">Users</h1>
-        <button class="flex gap-1 border border-1 xs:px-4 xs:py-4 rounded-lg items-center">
-            <span><x-svgs.filter class="w-4"/></span>
-            <span>Filters/Search</span>
-        </button>
-    </div>
+    <div x-data="{show:false}">
+        <div class="flex justify-between items-center my-4 sm:my-9 ">
+            <h1 class="font-bold text-2xl">Users</h1>
+            <button x-on:click="show =! show" class="flex gap-1 border border-1 xs:px-4 xs:py-4 rounded-lg items-center">
+                <span><x-svgs.filter class="w-4"/></span>
+                <span>Filters/Search</span>
+            </button>
+        </div>
 
-    <div class="border bg-gray-200 xs:py-3 xs:px-3 lg:py-5 lg:px-5 rounded-lg">
-        <form class="flex flex-col xs:gap-3 m-0" method="GET" action="{{ route('dashboard.users') }}">
-            <div class="flex xs:flex-col md:flex-row justify-between xs:gap-3">
-                <input type="text" id="large-input"
-                       class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500"
-                       name="search" value="{{ request()->query('search') ?? '' }}" placeholder="search...">
-                <select
-                    class="g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 "
-                    name="sortBy">
-                    <option value="-1" @selected(request('sortBy') === "-1")>Sort By</option>
-                    @foreach($columns as $column)
-                        <option value="{{ $column }}" @selected(request('sortBy') === $column)>{{ $column }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="flex xs:justify-start  md:justify-center">
+        <div  class="border bg-gray-200 xs:py-3 xs:px-3 lg:py-5 lg:px-5 rounded-lg"
+              x-transition:enter="slideInDown"
+              x-transition:enter-start=""
+              x-transition:enter-end=""
+              :class="show ? '' : 'hidden' "
+        >
+            <form class="flex flex-col xs:gap-3 m-0" method="GET" action="{{ route('dashboard.users') }}">
+                <div class="flex xs:flex-col md:flex-row justify-between xs:gap-3">
+                    <input type="text" id="large-input"
+                           class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500"
+                           name="search" value="{{ request()->query('search') ?? '' }}" placeholder="search...">
+                    <select
+                        class="g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 "
+                        name="sortBy">
+                        <option value="-1" @selected(request('sortBy') === "-1")>Sort By</option>
+                        @foreach($columns as $column)
+                            <option value="{{ $column }}" @selected(request('sortBy') === $column)>{{ $column }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="flex xs:justify-start  md:justify-center">
 
                     <div class="flex xs:flex-col xs:justify-start md:flex-row md:flex-wrap  items-start mb-4 gap-2">
 
@@ -49,15 +55,17 @@
 
                         @endforeach
                     </div>
-            </div>
-            <div class="">
-                <button type="submit"
-                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                    Apply
-                </button>
-            </div>
-        </form>
+                </div>
+                <div class="">
+                    <button type="submit"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        Apply
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
+
 
 
     <div class="relative overflow-x-auto mt-4">
