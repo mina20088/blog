7afaa59a20b89b09d33fef0ffdbcce64478f1cea
Mina Remon Shaker <minakiroollos@gmail.com>
2025-10-05 23:45:34 +0300

    Add compiled Blade label component view cache file
    - Create compiled PHP cache file for label Blade component
    - Add label element with dynamic attributes output using <?php echo e($attributes); ?>
    - Add for attribute binding to associate label with input element using <?php echo e($for); ?>
    - Set static CSS classes: block display, mb-2 bottom margin, text-base font size
    - Add font-bold weight and text-gray-900 color styling
    - Render label content using <?php echo e($content); ?>
    - Add PATH comment directive pointing to C:\Users\minar\projects\blog\resources\views/components/label.blade.php
    - Format label element across multiple lines for readability

---
+++ storage/framework/views/d5a127e1528d6beb44f19fd74ac5ad62.php
@@ -0,0 +1,11 @@
+
+<label
+    <?php echo e($attributes); ?>
+
+    for="<?php echo e($for); ?>"
+    class="block mb-2 text-base font-bold text-gray-900"
+>
+      <?php echo e($content); ?>
+
+</label>
+<?php /**PATH C:\Users\minar\projects\blog\resources\views/components/label.blade.php ENDPATH**/ ?>
