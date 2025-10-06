56e7f39cbb07bb44d625d9ad16713c9e424ef793
Mina Remon Shaker <minakiroollos@gmail.com>
2025-10-06 18:10:23 +0300

    feat(views): add phone input field to user profile form
    Add international phone number input field to the user personal information card component. This enhancement allows users to provide their contact phone numbers during profile creation with proper formatting and validation support.
    
    Changes:
    - Add phone input field with label component in user-create-personal-information-card.blade.php
    - Configure input with type="tel", id="phone", name="phone_number"
    - Apply !w-full class to ensure full-width display within intl-tel-input container
    - Change grid layout from grid-cols-2 to grid-flow-row for flexible single-column layout
    - Fix profile_image input padding class from p-0 to !p-0 for consistent styling override
    - Update compiled Blade cache with new component structure
    
    Technical details:
    - Phone input field uses id="phone" to match JavaScript intlTelInput initialization
    - Input name set to "phone_number" for form submission
    - Layout change accommodates international phone input widget requirements
    - Important (!important) Tailwind classes ensure proper component styling precedence
    
    This change integrates with the previously implemented intl-tel-input library to provide a complete international phone number collection solution in user registration and profile forms.

--- storage/framework/views/fb9f57937a78647c6f2c2bbdd4fea89d.php
+++ storage/framework/views/fb9f57937a78647c6f2c2bbdd4fea89d.php
@@ -240,14 +240,14 @@
 <?php endif; ?>
             <?php if (isset($component)) { $__componentOriginalc2fcfa88dc54fee60e0757a7e0572df1 = $component; } ?>
 <?php if (isset($attributes)) { $__attributesOriginalc2fcfa88dc54fee60e0757a7e0572df1 = $attributes; } ?>
-<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.input','data' => ['type' => 'file','id' => 'profile_image','class' => 'p-0','name' => 'profile_picture']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.input','data' => ['type' => 'file','id' => 'profile_image','class' => '!p-0','name' => 'profile_picture']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
 <?php $component->withName('input'); ?>
 <?php if ($component->shouldRender()): ?>
 <?php $__env->startComponent($component->resolveView(), $component->data()); ?>
 <?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
 <?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
 <?php endif; ?>
-<?php $component->withAttributes(['type' => 'file','id' => 'profile_image','class' => 'p-0','name' => 'profile_picture']); ?>
+<?php $component->withAttributes(['type' => 'file','id' => 'profile_image','class' => '!p-0','name' => 'profile_picture']); ?>
 <?php echo $__env->renderComponent(); ?>
 <?php endif; ?>
 <?php if (isset($__attributesOriginalc2fcfa88dc54fee60e0757a7e0572df1)): ?>
@@ -536,10 +536,51 @@
 
     </section>
 
-    <section class="grid grid-cols-2 md:gap-3">
+    <section class="grid grid-flow-row md:gap-3">
+        <section>
+            <?php if (isset($component)) { $__componentOriginald8ba2b4c22a13c55321e34443c386276 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginald8ba2b4c22a13c55321e34443c386276 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.label','data' => ['for' => 'phone','content' => 'Phone']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('label'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['for' => 'phone','content' => 'Phone']); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginald8ba2b4c22a13c55321e34443c386276)): ?>
+<?php $attributes = $__attributesOriginald8ba2b4c22a13c55321e34443c386276; ?>
+<?php unset($__attributesOriginald8ba2b4c22a13c55321e34443c386276); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginald8ba2b4c22a13c55321e34443c386276)): ?>
+<?php $component = $__componentOriginald8ba2b4c22a13c55321e34443c386276; ?>
+<?php unset($__componentOriginald8ba2b4c22a13c55321e34443c386276); ?>
+<?php endif; ?>
+            <?php if (isset($component)) { $__componentOriginalc2fcfa88dc54fee60e0757a7e0572df1 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginalc2fcfa88dc54fee60e0757a7e0572df1 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.input','data' => ['type' => 'tel','id' => 'phone','name' => 'phone_number','class' => '!w-full']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('input'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['type' => 'tel','id' => 'phone','name' => 'phone_number','class' => '!w-full']); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginalc2fcfa88dc54fee60e0757a7e0572df1)): ?>
+<?php $attributes = $__attributesOriginalc2fcfa88dc54fee60e0757a7e0572df1; ?>
+<?php unset($__attributesOriginalc2fcfa88dc54fee60e0757a7e0572df1); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalc2fcfa88dc54fee60e0757a7e0572df1)): ?>
+<?php $component = $__componentOriginalc2fcfa88dc54fee60e0757a7e0572df1; ?>
+<?php unset($__componentOriginalc2fcfa88dc54fee60e0757a7e0572df1); ?>
+<?php endif; ?>
+        </section>
 
     </section>
-
 
 
  <?php echo $__env->renderComponent(); ?>
@@ -552,4 +593,6 @@
 <?php $component = $__componentOriginal53747ceb358d30c0105769f8471417f6; ?>
 <?php unset($__componentOriginal53747ceb358d30c0105769f8471417f6); ?>
 <?php endif; ?>
+
+
 <?php /**PATH C:\Users\minar\projects\blog\resources\views/components/users/user-create-personal-information-card.blade.php ENDPATH**/ ?>
